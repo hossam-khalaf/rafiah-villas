8e94a6f3897d0c28ddd4a885d886e1e66dfa58cb
Hossam <hossam@Hossams-MacBook-Air-2.local>
2026-05-02 14:58:43 +0700

    Fix Sanity Studio client rendering

--- src/app/[locale]/studio/[[...index]]/page.tsx
+++ src/app/[locale]/studio/[[...index]]/page.tsx
@@ -1,8 +1,13 @@
 'use client';
 
-import { NextStudio } from 'next-sanity/studio';
+import dynamic from 'next/dynamic';
 import config from '../../../../../sanity.config';
 import { useParams } from 'next/navigation';
+
+const NextStudio = dynamic(
+  () => import('next-sanity/studio').then((mod) => mod.NextStudio),
+  { ssr: false }
+);
 
 export default function StudioPage() {
   const params = useParams();
